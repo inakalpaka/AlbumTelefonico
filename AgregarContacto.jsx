2c53926464e9a74ae87318a5a9609d60fca6e5af
inakalpaka <67813256+inakalpaka@users.noreply.github.com>
2024-08-14 14:18:03 -0300

    Add files via upload

--- AgregarContacto.jsx
+++ AgregarContacto.jsx
@@ -16,52 +16,53 @@ const AgregarContacto = () => {
     setTelefono(event.target.value);
   };
   const Guardar = () => {
-    if(nombre.length > 100){
-        console.log("el nombre es demaciado largo");
+    if (nombre.length > 100) {
+      console.log("el nombre es demaciado largo");
     }
-    else if(nombre.length < 5){
-        console.log("el nombre es demaciado corto");
+    else if (nombre.length < 5) {
+      console.log("el nombre es demaciado corto");
     }
-    else if(apellido.length > 100){
-        console.log("el apellido es demaciado largo");
+    else if (apellido.length > 100) {
+      console.log("el apellido es demaciado largo");
     }
-    else if(nombre.length < 5){
-        console.log("el apellido es demaciado corto");
+    else if (nombre.length < 5) {
+      console.log("el apellido es demaciado corto");
     }
-    else if(telefono == "351 123 4567"){
-        console.log("el numero no es valido");
-    }else{
-        if(nombre == nobreEnBaseDeDatos && apellido == apellidoEnBaseDeDatos){ //comprobar si en nombre que quiero cargar existe en la base de datos 
-            if(numero == numeroEnBaseDeDatos){
-                Swal.fire({
-                    title: "El Contacto existe deseas remoplazar el numero anterior?",
-                    showDenyButton: true,
-                    confirmButtonText: "Remplazar",
-                    denyButtonText: `Cancelar`
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                      //remplazar numero viejo por numero nuevo en la base de datos
-                      Swal.fire("Listo!", "Numero Remplazado", "Listo");
-                  }});
-                  
+    else if (telefono == "351 123 4567") {
+      console.log("el numero no es valido");
+    } else {
+      if (nombre == nobreEnBaseDeDatos && apellido == apellidoEnBaseDeDatos) { //comprobar si en nombre que quiero cargar existe en la base de datos 
+        if (numero == numeroEnBaseDeDatos) {
+          Swal.fire({
+            title: "El Contacto existe deseas remoplazar el numero anterior?",
+            showDenyButton: true,
+            confirmButtonText: "Remplazar",
+            denyButtonText: `Cancelar`
+          }).then((result) => {
+            if (result.isConfirmed) {
+              //remplazar numero viejo por numero nuevo en la base de datos
+              Swal.fire("Listo!", "Numero Remplazado", "Listo");
             }
-        }else{
-            //agregar el nuevo contacto a la base de datos
-            Swal.fire({
-                title: "Felicidades!",
-                text: "Tu nuevo contacto se a guardado con exito!",
-                icon: "success"
-              });
-        }
+          });
 
-        console.log("el nombre es: ");
-        console.log(nombre);
-        console.log("el apellido es: ");
-        console.log(apellido);
-        console.log("el telefono es: ");
-        console.log(telefono);
-        //guardar el json
-        //subirlo a la base de datos local
+        }
+      } else {
+        //agregar el nuevo contacto a la base de datos
+        Swal.fire({
+          title: "Felicidades!",
+          text: "Tu nuevo contacto se a guardado con exito!",
+          icon: "success"
+        });
+      }
+
+      console.log("el nombre es: ");
+      console.log(nombre);
+      console.log("el apellido es: ");
+      console.log(apellido);
+      console.log("el telefono es: ");
+      console.log(telefono);
+      //guardar el json
+      //subirlo a la base de datos local
     }
     console.log("el nombre es: ");
     console.log(nombre);
@@ -70,70 +71,70 @@ const AgregarContacto = () => {
     console.log("el telefono es: ");
     console.log(telefono);
   }
- /* return (
-<div>
-      <h1>Formulario</h1>
-      <input
-        type="text"
-        value={nombre}
-        onChange={nombreChange}
-      />
-      <> </>
-      <input
-        type="text"
-        value={apellido}
-        onChange={apellidoChange}
-      />
-      <></>
-      <input
-        type="text"
-        value={telefono}
-        onChange={telefonoChange}
-      />
-      <></>
-
-      <button onClick={Guardar}>Guardar</button>
-
-    </div>
-    
-  );*/
-  return(
+  /* return (
+ <div>
+       <h1>Formulario</h1>
+       <input
+         type="text"
+         value={nombre}
+         onChange={nombreChange}
+       />
+       <> </>
+       <input
+         type="text"
+         value={apellido}
+         onChange={apellidoChange}
+       />
+       <></>
+       <input
+         type="text"
+         value={telefono}
+         onChange={telefonoChange}
+       />
+       <></>
+ 
+       <button onClick={Guardar}>Guardar</button>
+ 
+     </div>
+     
+   );*/
+  return (
     <form onSubmit={Guardar}>
-    <div>
+      <div>
         <label>
-            Nombre:
-            <input 
-                type="text" 
-                value={nombre} 
-                onChange={(e) => setNombre(e.target.value)} 
-                required 
-            />
+          Nombre:
+          <input
+            type="text"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            required
+          />
         </label>
-    </div>
-    <div>
+      </div>
+      <div>
         <label>
-            Apellido:
-            <input 
-                type="text" 
-                value={apellido} 
-                onChange={(e) => setApellido(e.target.value)} 
-                required 
-            />
+          Apellido:
+          <input
+            type="text"
+            value={apellido}
+            onChange={(e) => setApellido(e.target.value)}
+            required
+          />
         </label>
-    </div>
-    <div>
+      </div>
+      <div>
         <label>
-            Número de Teléfono:
-            <input 
-                type="tel" 
-                value={telefono} 
-                onChange={(e) => setTelefono(e.target.value)} 
-                required 
-            />
+          Número de Teléfono:
+          <input
+            type="tel"
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)}
+            required
+          />
         </label>
-    </div>
-    <button type="submit">Guardar</button>
-</form>
+      </div>
+      <button type="submit">Guardar</button>
+    </form>
 
   );
 };
--- Conexiondb.jsx
+++ Conexiondb.jsx
@@ -1,39 +1,59 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Conexionbd = (...args) => {
+const Conexionbd = ({ nombre, apellido, telefono, excepcion }) => {
+    // utilizar destructuracion
     const [posts, setPosts] = useState([]);
-
-
-            axios.get("http://localhost:3000/posts")
+    const obtenerPosteos = (posts) => {
+        axios.get("http://localhost:3000/posts")    //obtengo los posts de la base de datos
             .then((response) => {
                 setPosts(response.data);
             })
             .catch((error) => {
-                console.error("Error fetching posts:", error);
+                console.error("Error haciendo get", error);
             });
-        if(args == []){
-            return posts;
-        }else{
-            for( i in posts){
-                if(i.nombre == args[0] && i.apellido == args[1]){
-                    if(i.numTelefono == args[2]){
-                        return "numero ya regisnrado";
-                    }else{
-                        return "contacto registrado, numero distinto";
-                    }
-                }else{
-                    try{
-                    axios.post('${ http://localhost:3000/posts}, ${{nombre: args[0],apellido: args[1],numTelefono: args[2]}}')
-                    return "numero agregado";
-                    }
-                    .catch((error) => {
-                        console.error("Error aciendo post:", error);
-                    });
-                }
-            }
+    }
+    const editarPosteo = ({telefono, i }) => {
+        try {
+            axios.patch(`http://localhost:3000/posts/${i.id}`, { numero: telefono });
+        } catch (error) {
+            console.error('Error al verificar o actualizar:', error);
+        }
+    }
 
+    obtenerPosteos();
+
+    if (nombre == "") { // me pregunto si recibi algun argumento 
+        return posts; // si no recibi nada retorno los posteos de la base de datos
+    } else {
+        for (i in posts) {
+            if (i.nombre == nombre && i.apellido == apellido) { // compruebo si el contacto existe
+                if (i.numTelefono == telefono) { // me pregunto si el numero tambien coiside 
+                    return "numero ya registrado";
+                } else {
+                    if (excepcion == true) {
+                        editarPosteo(telefono, i);
+                    }else{
+                        return "el contacto existe con otro numero"
+                    }
+                    // Llamar a la función con los datos necesarios
+                }
+                return "numero actualizado";
+            } else {
+            try {
+                axios.post('${ http://localhost:3000/posts}, ${{nombre: nombre,apellido: apellido,numTelefono: telefono}}')
+                return "numero agregado";
+            } catch {
+                console.error("Error añadiendo numero:");
+            };
+        }
+    }
+    // cursera cursos con descuetos para tercer mundistas
+    // improvisacion de ex-baterista rhcp improvisacion
+    // utilizar pico css para darle estilo
     return posts;
+};
+
 };
 
 export default Conexionbd;
